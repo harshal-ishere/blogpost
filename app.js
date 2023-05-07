518614906eb37bcbf3bf485feab0db75dc3a86ed
sync-ster <harshal011kamble@gmail.com>
2023-05-08 01:34:22 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,82 @@
+//jshint esversion:6
+
+const express = require("express");
+const mongoose=require("mongoose");
+var _=require("lodash");
+
+const app = express();
+app.set('view engine', 'ejs');
+mongoose.connect("mongodb+srv://harshal011k:b4icunokmidown@cluster0.ls2zwnz.mongodb.net/blogDB")
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+let usSchema={
+  name:String
+};
+let Us=mongoose.model("Us",usSchema);
+let aboutContent=new Us({
+  name:"Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui."
+});
+let contactContent=new Us({
+  name:"Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero."
+});
+let homeStartingContent=new Us({
+  name:"Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing."
+});
+
+let blogSchema={
+  name:String,
+  data:String
+};
+let Blog=mongoose.model("Blog",blogSchema);
+
+app.get("/",(req,res)=>{
+  Blog.find({})
+  .then((foundItem)=>{
+      res.render("home",{para1:homeStartingContent,item:foundItem});
+      res.redirect("/");
+  })
+  .catch((err)=>{
+    console.log(err);
+  });
+});
+
+app.post("/compose",(req,res)=>{
+ let post={
+    title:req.body.postTitle,
+    body:req.body.postBody,
+  };
+  let blog=new Blog({
+    name:post.title,
+    data:post.body
+  });
+  blog.save();
+  res.redirect("/");
+});
+
+//EXPRESS ROUTING PARAMETERS.....
+app.get("/posts/:postId",(req,res)=>{
+   let requestedPostId = req.params.postId;
+   Blog.findOne({_id:requestedPostId})
+   .then((post)=>{
+    res.render("post",{title:post,data:post});
+   })
+   .catch((err)=>{
+    console.log(err);
+   });
+});
+
+app.listen(3000,()=> {
+  console.log("Server started on port 3000");
+});
+
+app.get("/about",(req,res)=>{
+  res.render("about",{about:aboutContent});
+});
+app.get("/contact",(req,res)=>{
+  res.render("contact",{contact:contactContent});
+});
+app.get("/compose",(req,res)=>{
+  res.render("compose");
+});
